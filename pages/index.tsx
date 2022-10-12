e498fef8c41493ff36043ac5b3041ddd59a8dd2e
Ryoneme2 <worknarair6@gmail.com>
2022-10-12 19:12:43 +0700

    chore : config tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <>
-      <h1 className='text-red-200'>Hello slog</h1>
+      <h1 className='text-red-500'>Hello slog</h1>
     </>
   );
 };
